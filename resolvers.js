6d0195619be96622fd99e8a2b86e29835a803178
Csillag61 <csillagok@gmail.com>
2021-02-06 20:40:20 -0500

    updated packages

---
+++ resolvers.js
@@ -0,0 +1,21 @@
+const { AuthenticationError } = require("apollo-server")
+
+const user = {
+    
+    name: "Csillag",
+    email: "csillagok@gmail.com",
+    picture: "https://cloudinary.com/asdf"
+}
+
+const authenticated = next => (root, args, ctx, info)=> {
+    if (!ctx.currentUser){
+        throw new AuthenticationError('You must be loged in')
+    }
+    return next(root, args, ctx, info)
+}
+
+module.exports= {
+    Query:{
+        me: authenticated((root, args, ctx, info) => ctx.currentUser)
+    }
+}
