c656d43725e13ed0baa3f3f54eb897d0197157e9
Csillag61 <csillagok@gmail.com>
2021-02-14 11:43:36 -0500

    updated pinIcon.js

--- client/src/components/PinIcon.js
+++ client/src/components/PinIcon.js
@@ -1,4 +1,7 @@
-import React from "react";
-// import PlaceTwoTone from "@material-ui/icons/PlaceTwoTone";
+import React from 'react';
+import {PlaceTwoTone} from '@material-ui/icons/PlaceTwoTone';
 
-export default () => <div>PinIcon</div>;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ size, color, onClick }) => (
+  <PlaceTwoTone onClick={onClick} style={{ fontSize: size, color }} />
+);
--- client/src/graphql/subscriptions.js
+++ client/src/graphql/subscriptions.js
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 export const PIN_ADDED_SUBSCRIPTION = gql`
   subscription {
